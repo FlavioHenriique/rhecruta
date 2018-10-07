a80a1c3d9522b46f01976b70ed5033960698edf5
FlavioHenriique <flaviohenrique638@gmail.com>
2018-10-07 20:56:59 -0300

    Implementando e testando classes para envio de emails relacionados a lembretes de horário de entevistas e finalização do recrutamento

---
+++ projeto/src/main/java/br/edu/ifpb/dacrhecruta/javamail/EnviaEmail.java
@@ -0,0 +1,58 @@
+package br.edu.ifpb.dacrhecruta.javamail;
+
+import java.util.Properties;
+import javax.ejb.Stateless;
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@Stateless
+public class EnviaEmail {
+
+    public EnviaEmail(){
+
+    }
+
+    public void enviar(String destinatario, String titulo, String corpo) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(props,
+                new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("dac20181@gmail.com",
+                        "D2!dac2018");
+            }
+        });
+        session.setDebug(true);
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("dac20181@gmail.com"));
+
+            Address[] toUser = InternetAddress.parse(destinatario);
+
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject(titulo);
+            message.setText(corpo);
+
+            Transport.send(message);
+
+            System.out.println("Feito!!!");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
