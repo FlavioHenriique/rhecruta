da7b7ce0f86dc8f3ec66f9ff14576b38461fee2f
Julierme Heinstein <julierme.jadon@outlook.com>
2018-10-10 13:54:38 -0300

    Implementando JSF
    Incrementando paginas
    Criando menu

--- run.bat
+++ run.bat
@@ -1,9 +1,11 @@
+cd C:\Users\Lestat\Developer\rhecruta
 docker build -t rhecruta/banco banco/
 
-cd projeto/
+cd projeto\
 mvn clean package
 
 docker build -t rhecruta/app .
 
-docker run -d  -p 5433:5432 --name bdrhecruta rhecruta/banco
+cd ..
+docker run -d  -p 5433:5432 --name bdrhecruta /banco
 docker run  -p 8081:8080 --link bdrhecruta:host-banco rhecruta/app
