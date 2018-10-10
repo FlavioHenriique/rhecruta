5086ce6c0c2bdbf5ad7129cdb95d09e04e1cf18f
FlavioHenriique <flaviohenrique638@gmail.com>
2018-10-10 17:45:34 -0300

    Adicionando arquivo para o menu e incluindo em todas as paginas xhtml

--- projeto/src/main/java/br/edu/ifpb/dacrhecruta/services/CandidatoService.java
+++ projeto/src/main/java/br/edu/ifpb/dacrhecruta/services/CandidatoService.java
@@ -1,15 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.edu.ifpb.dacrhecruta.services;
 
 import br.edu.ifpb.dacrhecruta.dao.interfaces.CandidatoDaoIF;
 import br.edu.ifpb.dacrhecruta.domain.Candidato;
 import br.edu.ifpb.dacrhecruta.facade.CandidatoFacade;
 import br.edu.ifpb.dacrhecruta.resource.CandidatoResource;
+import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -18,8 +15,8 @@ import javax.inject.Named;
  * @author Lestat
  */
 @Named
-@RequestScoped
-public class CandidatoService implements CandidatoFacade {
+@SessionScoped
+public class CandidatoService implements CandidatoFacade, Serializable {
 
     private Candidato candidato = new Candidato();
     @Inject
@@ -28,8 +25,10 @@ public class CandidatoService implements CandidatoFacade {
     @Override
     public String autenticar() {
         if (dao.autenticar(candidato.getEmail(), candidato.getSenha()) != null) {
+            candidato = dao.buscar(candidato);
             return "home.xhtml";
         } else {
+            candidato = new Candidato();
             return "welcome.xhtml";
         }
     }
@@ -56,6 +55,12 @@ public class CandidatoService implements CandidatoFacade {
     public String deletar() {
         dao.deletar(candidato);
         return "home.xhtml";
+    }
+
+    public String sair() {
+        System.out.println("sair");
+        candidato = new Candidato();
+        return "welcome.xhtml";
     }
 
     public Candidato getCandidato() {
